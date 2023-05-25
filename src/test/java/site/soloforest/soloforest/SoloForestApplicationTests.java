5e781d116abeb35a5097f1c00c2fb4e704cd9ab4
깃chan0521 <aflpq369@gmail.com>
2023-05-25 18:46:24 +0900

    메Main

---
+++ src/test/java/site/soloforest/soloforest/SoloForestApplicationTests.java
@@ -0,0 +1,13 @@
+package site.soloforest.soloforest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloForestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
