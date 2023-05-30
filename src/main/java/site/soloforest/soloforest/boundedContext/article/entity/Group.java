915cc0ed1740c9952248785c0ca4e56f27124af0
깃ChertHyeon <r4560798@naver.com>
2023-05-30 14:27:23 +0900

    feat : Group엔티티 NotBlank 적용을 위한 import 추가

--- src/main/java/site/soloforest/soloforest/boundedContext/article/entity/Group.java
+++ src/main/java/site/soloforest/soloforest/boundedContext/article/entity/Group.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
