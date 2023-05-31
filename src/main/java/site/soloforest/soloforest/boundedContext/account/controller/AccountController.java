a60a7121586c92f12ddd08b3de4e1be5da9f1599
elephantLikesApple <elephant890@naver.com>
2023-06-01 04:51:36 +0900

    test : localhost ui 테스트용 계정 생성 가능

--- src/main/java/site/soloforest/soloforest/boundedContext/account/controller/AccountController.java
+++ src/main/java/site/soloforest/soloforest/boundedContext/account/controller/AccountController.java
@@ -1,17 +1,42 @@
 package site.soloforest.soloforest.boundedContext.account.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
+import site.soloforest.soloforest.boundedContext.account.entity.Account;
+import site.soloforest.soloforest.boundedContext.account.repository.AccountRepository;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/account")
 public class AccountController {
+	@Autowired
+	private final AccountRepository accountRepository;
+	@Autowired
+	private final PasswordEncoder passwordEncoder;
+
 	@GetMapping("/login")
+	@PreAuthorize("isAnonymous()")
 	public String showLogin() {
 		return "/account/login";
+	}
+
+	@GetMapping("/signUp")
+	@PreAuthorize("isAnonymous()")
+	public String signup() {
+
+		Account account = Account.builder()
+			.username("usertest")
+			.password(passwordEncoder.encode("test1"))
+			.nickname("for test")
+			.email("test@test.com")
+			.build();
+		this.accountRepository.save(account);
+		return "redirect:/account/login";
 	}
 }
