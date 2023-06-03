039a227f0231010303b24a1c2ebc94796a268939
elephantLikesApple <elephant890@naver.com>
2023-06-03 14:51:10 +0900

    bug : (진행중) 회원가입 즉시 로그인세션 생성 안되는 문제

--- src/main/java/site/soloforest/soloforest/boundedContext/account/service/AccountService.java
+++ src/main/java/site/soloforest/soloforest/boundedContext/account/service/AccountService.java
@@ -1,5 +1,12 @@
 package site.soloforest.soloforest.boundedContext.account.service;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +21,8 @@ public class AccountService {
 	private final AccountRepository accountRepository;
 	private final PasswordEncoder passwordEncoder;
 
+	private final AuthenticationManager authenticationManager;
+
 	public Account singup(AccountDTO dto) {
 		Account account = Account.builder()
 			.username(dto.getUsername())
@@ -23,6 +32,37 @@ public class AccountService {
 			.address(dto.getAddress())
 			.build();
 		Account savedAccount = accountRepository.save(account);
+		authenticateAccountAndSetSession(savedAccount);
 		return savedAccount;
 	}
+
+	private void authenticateAccountAndSetSession(Account account) {
+		// 사용자 인증
+		UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(account.getUsername(),
+			account.getPassword(), account.getGrantedAuthorities());
+		System.out.println(token);
+		try {
+			// AuthenticationManager 에 token 을 넘기면 UserDetailsService 가 받아 처리하도록 한다.
+			Authentication authentication = authenticationManager.authenticate(token);
+			// 실제 SecurityContext 에 authentication 정보를 등록한다.
+			SecurityContextHolder.getContext().setAuthentication(authentication);
+		} catch (DisabledException | LockedException | BadCredentialsException e) {
+			String status;
+			if (e.getClass().equals(BadCredentialsException.class)) {
+				status = "invalid-password";
+			} else if (e.getClass().equals(DisabledException.class)) {
+				status = "locked";
+			} else if (e.getClass().equals(LockedException.class)) {
+				status = "disable";
+			} else {
+				status = "unknown";
+			}
+			System.out.println("catch" + status);
+		}
+
+		// 보안 세션 정보에 사용자 인증 객체 설정
+		SecurityContextHolder.getContext().setAuthentication(token);
+		System.out.println("security" + SecurityContextHolder.getContext().getAuthentication());
+	}
+
 }
