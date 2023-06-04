8a676c389f2c7f8c2341f66e5859c747aaeed285
ujineeee <yujin11006@naver.com>
2023-06-05 03:07:59 +0900

    test : share controller 게시글 작성 테스트

--- src/test/java/site/soloforest/soloforest/boundedContext/article/controller/ShareControllerTests.java
+++ src/test/java/site/soloforest/soloforest/boundedContext/article/controller/ShareControllerTests.java
@@ -1,5 +1,7 @@
 package site.soloforest.soloforest.boundedContext.article.controller;
 
+import static org.hamcrest.Matchers.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -65,5 +67,36 @@ public class ShareControllerTests {
 		resultActions
 			.andExpect(status().is3xxRedirection())
 			.andExpect(redirectedUrl("/main"));
+	}
+
+	@Test
+	@DisplayName("게시글 작성 폼 처리")
+	void t004() throws Exception {
+		ResultActions resultActions = mvc
+			.perform(post("/article/share/community/create")
+				.with(csrf())
+				.param("subject", "test Subject")
+				.param("content", "test Content")
+			)
+			.andDo(print());
+
+		resultActions
+			.andExpect(handler().handlerType(ShareController.class))
+			.andExpect(handler().methodName("create"))
+			.andExpect(redirectedUrlPattern("/article/share/detail/*"));
+	}
+
+	@Test
+	@DisplayName("게시글 상세 페이지 잘못 된 경로 - 에러 페이지 호출")
+	void t005() throws Exception {
+		ResultActions resultActions = mvc
+			.perform(get("/article/share/detail/-1"))
+			.andDo(print());
+
+		resultActions
+			.andExpect(handler().handlerType(ShareController.class))
+			.andExpect(handler().methodName("detail"))
+			.andExpect(status().is2xxSuccessful())
+			.andExpect(content().string(containsString("404 ERROR")));
 	}
 }
