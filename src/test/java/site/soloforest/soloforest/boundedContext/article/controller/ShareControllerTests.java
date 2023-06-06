358ddff5b96f976a1659ffba9c39e1e12d669a72
ujineeee <yujin11006@naver.com>
2023-06-07 01:31:58 +0900

    test : share controller 게시글 수정 테스트

--- src/test/java/site/soloforest/soloforest/boundedContext/article/controller/ShareControllerTests.java
+++ src/test/java/site/soloforest/soloforest/boundedContext/article/controller/ShareControllerTests.java
@@ -70,7 +70,7 @@ public class ShareControllerTests {
 	}
 
 	@Test
-	@DisplayName("게시글 작성 폼 처리")
+	@DisplayName("게시글 작성 폼 처리 테스트")
 	void t004() throws Exception {
 		ResultActions resultActions = mvc
 			.perform(post("/article/share/community/create")
@@ -87,7 +87,7 @@ public class ShareControllerTests {
 	}
 
 	@Test
-	@DisplayName("게시글 상세 페이지 잘못 된 경로 - 에러 페이지 호출")
+	@DisplayName("게시글 상세 페이지 잘못 된 경로 - 에러 페이지 호출 테스트")
 	void t005() throws Exception {
 		ResultActions resultActions = mvc
 			.perform(get("/article/share/detail/-1"))
@@ -96,7 +96,23 @@ public class ShareControllerTests {
 		resultActions
 			.andExpect(handler().handlerType(ShareController.class))
 			.andExpect(handler().methodName("detail"))
-			.andExpect(status().is2xxSuccessful())
 			.andExpect(content().string(containsString("404 ERROR")));
+	}
+
+	@Test
+	@DisplayName("게시글 수정 폼 처리 테스트")
+	void t006() throws Exception {
+		ResultActions resultActions = mvc
+			.perform(post("/article/share/modify/1")
+				.with(csrf())
+				.param("subject", "test Subject")
+				.param("content", "test Content")
+			)
+			.andDo(print());
+
+		resultActions
+			.andExpect(handler().handlerType(ShareController.class))
+			.andExpect(handler().methodName("modify"))
+			.andExpect(redirectedUrl("/article/share/detail/1"));
 	}
 }
