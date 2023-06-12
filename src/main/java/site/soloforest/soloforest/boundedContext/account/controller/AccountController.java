1da00192ffd164d51eb5e8f48d4a18837db4445c
elephantLikesApple <elephant890@naver.com>
2023-06-12 14:43:45 +0900

    design : id/pw찾기 페이지 작성

--- src/main/java/site/soloforest/soloforest/boundedContext/account/controller/AccountController.java
+++ src/main/java/site/soloforest/soloforest/boundedContext/account/controller/AccountController.java
@@ -97,4 +97,10 @@ public class AccountController {
 		}
 		return accountService.report(id);
 	}
+
+	@GetMapping("/findAccount")
+	@PreAuthorize("isAnonymous()")
+	public String find() {
+		return "account/find_account";
+	}
 }
